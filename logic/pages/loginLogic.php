ce80fd080595492232c6afda46f93d92ec697982
unknown <chicokds@gmail.com>
2023-09-27 13:36:56 +0200

    Changed logic to use POST

--- logic/pages/loginLogic.php
+++ logic/pages/loginLogic.php
@@ -4,7 +4,7 @@ require_once "..\logic\classes\userClass.php";
 require_once "..\db\DBConnection.php";
 require_once "..\logic\sessions.php";
 
-$userData=(new DBConnection())->readUser(filter_var($_GET['email'], FILTER_SANITIZE_EMAIL), htmlspecialchars($_GET['password']));
+$userData=(new DBConnection())->readUser(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL), htmlspecialchars($_POST['password']));
 
 $loginUser=new User(
     ["email", $userData["email"]],
